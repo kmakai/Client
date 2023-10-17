6b091fbb1552552957c8309df500ca3dc4878357
kmakai <makaikwag@gmail.com>
2023-10-17 16:25:45 -0400

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,19 @@ export class AppComponent {
   getList() {
     this.coffeeService.getCoffees().subscribe((data) => {
       this.coffeeList = data;
+      this.sortList();
     });
   }
   onCoffeeAdded(c: Coffee): void {
     this.coffeeService.getCoffees().subscribe((coffees) => {
       this.coffeeList = coffees;
+      this.sortList();
+    });
+  }
+
+  sortList() {
+    this.coffeeList.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
     });
   }
 }
